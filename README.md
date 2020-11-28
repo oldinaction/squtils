af14226ccc94224663ada8321ec4041a8501ca5c
smalle <oldinaction@qq.com>
2020-11-28 14:33:56 +0800

    init

--- README.md
+++
@@ -1,4 +0,0 @@
-## smtools
-
-- `utils` java工具类
-- `core` 基于spring的常用写法
--- core/src/main/java/cn/aezo/core/service/IdWorker.java
+++
@@ -1,8 +0,0 @@
-package cn.aezo.core.service;
-
-/**
- * Created by smalle on 2019-04-03 13:41.
- */
-public interface IdWorker {
-    long getId();
-}
