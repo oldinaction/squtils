bebb18cc177882a12d55213dce065b2784ab0439
smalle <oldinaction@qq.com>
2021-02-07 20:00:11 +0800

    发送邮件

--- src/main/java/cn/aezo/utils/ext/spring/EmailService.java
+++ src/main/java/cn/aezo/utils/ext/spring/EmailService.java
@@ -1,5 +1,6 @@
 package cn.aezo.utils.ext.spring;
 
+import cn.aezo.utils.base.ValidU;
 import cn.hutool.extra.template.Template;
 import cn.hutool.extra.template.TemplateConfig;
 import cn.hutool.extra.template.TemplateEngine;
@@ -12,6 +13,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.mail.internet.MimeMessage;
 import java.util.Arrays;
 import java.util.Map;
@@ -25,15 +27,27 @@ import java.util.Map;
 @Slf4j
 public class EmailService {
 
-    private final JavaMailSender javaMailSender;
+    private static JavaMailSender javaMailSender;
+    private static String sender;
 
+    @Value("${spring.mail.properties.from:}")
+    private String from;
     @Value("${spring.mail.username:}")
-    private String sender;
+    private String username;
 
     @Lazy
     @Autowired(required = false)
     public EmailService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
+        EmailService.javaMailSender = javaMailSender;
+    }
+
+    @PostConstruct
+    public void init() {
+        if(ValidU.isNotEmpty(from)) {
+            EmailService.sender = from;
+        } else {
+            EmailService.sender = username;
+        }
     }
 
     /**
@@ -45,7 +59,7 @@ public class EmailService {
      * @param to 收件人
      * @return boolean
      */
-    public boolean sendEmail(String subject, String content, String... to) {
+    public static boolean sendEmail(String subject, String content, String... to) {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,true);
@@ -64,21 +78,30 @@ public class EmailService {
 
     /**
      * 基于模板发送邮件
-     * @param tpl 模板相对路径，基于 classpath:templates/email 目录
-     * @param context 模板填充值
      * @param subject 主题
+     * @param customEngine 使用的模板类型. 如：VelocityEngine.class、FreemarkerEngine.class
+     * @param tpl 模板相对路径，基于 classpath:/templates/email 目录
+     * @param context 模板填充值
      * @param to 收件人
      * @return boolean
      */
-    public boolean sendEmailByTpl(String tpl, Map<String, Object> context, String subject, String... to) {
+    public static boolean sendEmailByTpl(String subject, Class<? extends TemplateEngine> customEngine, String tpl, Map<String, Object> context, String... to) {
         try {
-            TemplateEngine engine = TemplateUtil.createEngine(new TemplateConfig("templates/email", TemplateConfig.ResourceMode.CLASSPATH));
-            Template template = engine.getTemplate("templates/email/" + tpl);
+            TemplateConfig templateConfig = new TemplateConfig("/templates/email", TemplateConfig.ResourceMode.CLASSPATH);
+            if(customEngine != null) {
+                templateConfig.setCustomEngine(customEngine);
+            }
+            TemplateEngine engine = TemplateUtil.createEngine(templateConfig);
+            Template template = engine.getTemplate(tpl);
             String content = template.render(context);
             return sendEmail(subject, content, to);
         } catch (Exception e) {
             log.error("邮件发送出错. 邮件内容：" + context + ". 收件人：" + Arrays.asList(to), e);
             return false;
         }
+    }
+
+    public static boolean sendEmailByTpl(String subject, String tpl, Map<String, Object> context, String... to) {
+        return sendEmailByTpl(subject, null, tpl, context, to);
     }
 }
