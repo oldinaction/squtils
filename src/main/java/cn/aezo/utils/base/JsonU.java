7992f7ca43c8775daabbefafa64ae97554aab596
smalle <oldinaction@qq.com>
2021-06-10 09:41:28 +0800

    up

--- src/main/java/cn/aezo/utils/base/JsonU.java
+++ src/main/java/cn/aezo/utils/base/JsonU.java
@@ -2,18 +2,37 @@ package cn.aezo.utils.base;
 
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created by smalle on 2017/12/30.
  */
+@Slf4j
 public class JsonU {
+
+    public static Map<String, Object> toMapSafe(String str) {
+        Map<String, Object> ret = JSONUtil.toBean(str, Map.class, true);
+        if(ret == null) {
+            ret = new HashMap<>();
+        }
+        return ret;
+    }
+
+    public static JSONArray toListSafe(String str) {
+        JSONArray ret = JSONUtil.toBean(str, JSONArray.class, true);
+        if(ret == null) {
+            ret = new JSONArray();
+        }
+        return ret;
+    }
 
     /**
      * 通过HTTP获取JSON数据（Map）
@@ -32,7 +51,7 @@ public class JsonU {
             }
             return JSONUtil.toBean(sb.toString(), Map.class);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("", e);
         }
         return null;
     }
@@ -53,7 +72,7 @@ public class JsonU {
             }
             return JSONUtil.toList((JSONArray) JSONUtil.toBean(sb.toString(), JSONArray.class, true), Map.class);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("", e);
         }
         return null;
     }
